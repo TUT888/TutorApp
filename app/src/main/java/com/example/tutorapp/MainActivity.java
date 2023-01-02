3604a440398dc3a5ef7693345c93c7394b874db7
Tat Uyen Tam <58362988+TUT888@users.noreply.github.com>
2023-01-02 23:43:13 +0700

    Update MainActivity

--- app/src/main/java/com/example/tutorapp/MainActivity.java
+++ app/src/main/java/com/example/tutorapp/MainActivity.java
@@ -35,11 +35,7 @@ import com.google.gson.Gson;
 import java.io.Serializable;
 
 public class MainActivity extends AppCompatActivity {
-//    public final static String URL = "http://192.168.1.8:8282"; // Tam url
-//    public final static String URL = "http://10.0.136.237"; ///Tien url
-//    public final static String URL = "http://172.16.12.110"; ///Tien url
-//    public final static String URL = "http://192.168.1.9:8080"; /// San url
-    public final static String URL = "http://10.35.63.73"; /// IP ITZone
+    public final static String URL = "http://192.168.1.8:8282";
 
     public final static String URL_IMAGE = URL +  "/image/";
     public static String CURRENT_LOGIN_AVATAR = "";
